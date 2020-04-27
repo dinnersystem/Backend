df8e386bcc4bfe32ac33920d404aa6af6e22cd46
lawrence910426 <lawrence910426@gmail.com>
2020-04-27 22:44:41 +0800

    Update show_dish.php

--- food/dish/show_dish.php
+++ food/dish/show_dish.php
@@ -8,31 +8,36 @@ function show_dish($sortby)
 
     $me = unserialize($_SESSION['me']);
     $dish = get_dish();
-    $department = unserialize($_SESSION["department"]);
+    $department = get_department();
     $factory = get_factory(false);
     
     foreach ($dlimit as $key => $row) {
         $dish[$key]->init_limit($row["last_update"], $row["sum"], $row["limit"]);
     }
     foreach ($flimit as $key => $row) {
-        if (array_key_exists($factory, $key)) {
+        if (array_key_exists($key, $factory)) {
             $factory[$key]->init_limit($row["last_update"], $row["sum"], $row["limit"]);
         }
     }
 
     foreach ($department as $dp) {
-        if (array_key_exists($factory, $dp->factory->id)) {
+        if (array_key_exists($dp->factory->id, $factory)) {
             $dp->factory = $factory[$dp->factory->id];
+        } else {
+            $dp->factory = null;
         }
     }
+
+    $ans = [];
     foreach ($dish as $d) {
         if ($department[$d->department->id]->factory != null) {
             $d->department = $department[$d->department->id];
+            $ans[] = $d;
         }
     }
 
     if ($sortby != "dish_id") {
-        usort($dish, function ($a, $b) {
+        usort($ans, function ($a, $b) {
             if ($a->best_seller && !$b->best_seller) {
                 return false;
             } elseif (!$a->best_seller && $b->best_seller) {
@@ -44,5 +49,5 @@ function show_dish($sortby)
             }
         });
     }
-    return $dish;
+    return $ans;
 }
